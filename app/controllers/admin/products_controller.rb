1649f97359c9b319af2ee1a64b6739dc33443dcf
Type in your name <Type in your email>
2016-11-08 09:11:16 +0700

    update and destroy product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,13 @@
 class Admin::ProductsController < ApplicationController
   load_and_authorize_resource
-  before_action :load_categories, only: [:new, :create]
+  before_action :load_categories, only: [:new, :create, :edit, :update]
 
   def new
+  end
+
+  def index
+    @products = Product.all.order("created_at DESC").paginate page: params[:page],
+      per_page: Settings.category.per_page
   end
 
   def create
@@ -12,15 +17,34 @@ class Admin::ProductsController < ApplicationController
         format.json {head :no_content}
         format.js
       else
-        format.json {render json: @category.errors.full_messages,
+        format.json {render json: @product.errors.full_messages,
           status: :unprocessable_entity}
       end
     end
   end
 
-  def index
-    @products = Product.all.order("created_at DESC").paginate page: params[:page],
-      per_page: Settings.category.per_page
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @product.update_attributes product_params
+        format.json {head :no_content}
+        format.json
+      else
+        format.json {render json: @product.errors.full_messages,
+          status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.js
+      format.html {redirect_to products_url}
+      format.json {head :no_content}
+    end
   end
 
   private
