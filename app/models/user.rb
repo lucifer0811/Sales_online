a83d248f3f0558b0887eddfd2626941c373f78d4
Type in your name <Type in your email>
2017-01-20 13:23:28 +0700

    send_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-    :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+    :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook]
   has_many :comments
   has_many :requests
   has_many :videos, dependent: :destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name:  "admin",
-  email: "admin@example.com",
+  email: "admin@gmail.com",
   password: "123456",
   password_confirmation: "123456",
   admin: true
